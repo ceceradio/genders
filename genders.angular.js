ed4a250bb35614a1e5ad504ed582890521ae0840
Charlie <charles.wagner@devfluid.com>
2015-04-30 22:36:44 -0400

    added an angular service to provide genders!

---
+++ genders.angular.js
@@ -0,0 +1,41 @@
+(function() {
+  angular.module('genders', [])
+  .service('genders', function() {
+    return [
+      "Agender",
+      "Androgyne",
+      "Androgynous",
+      "Bigender",
+      "Cis Female",
+      "Cis Feminine",
+      "Cis Male",
+      "Cis Masculine",
+      "Cis Woman",
+      "Demigirl",
+      "Demiguy",
+      "Female",
+      "Feminine",
+      "Femme",
+      "Genderqueer",
+      "Genderweird",
+      "Gender Fluid",
+      "Intergender",
+      "Intersex",
+      "Male",
+      "Man",
+      "Masculine",
+      "Neutrois",
+      "Nonbinary",
+      "Other",
+      "Pangender",
+      "Third Gender",
+      "Trans Female",
+      "Trans Male",
+      "Trans Man",
+      "Trans Feminine",
+      "Trans Femme",
+      "Trans Woman",
+      "Woman"
+    ];
+  });
+})();
